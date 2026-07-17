69e86a35c231cbe625d30272ad21cf07bf7d95ac
Ka0tick666 <benjaminnavarretessg1@gmail.com>
2026-07-17 17:40:26 -0400

    Actualización visual frontend

--- front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
+++ front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,8 +11,8 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Consultar Ordenes de compra 💰 test final"
-          description="Revisa las últimas oc realizadas para generar su despacho test final"
+          title="Consultar Ordenes de compra 💰"
+          description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
             setTablaCompras(true);
